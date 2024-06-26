d8f70ea579cd4d20ff1d35cc43967fdee568b5b4
Mayur Bhoyar <mayurbhoyar4455@gmail.com>
2024-06-26 10:57:32 +0530

    loginissuefixed

--- client/src/index.js
+++ client/src/index.js
@@ -9,15 +9,13 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <GoogleOAuthProvider clientId="11348802405-f8mp3d9tbf5f839mh7qf1k6oo9csec36.apps.googleusercontent.com">
-          <App />
-        </GoogleOAuthProvider>
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <AuthProvider>
+      <GoogleOAuthProvider clientId="11348802405-f8mp3d9tbf5f839mh7qf1k6oo9csec36.apps.googleusercontent.com">
+        <App />
+      </GoogleOAuthProvider>
+    </AuthProvider>
+  </BrowserRouter>
 );
 
 reportWebVitals();
