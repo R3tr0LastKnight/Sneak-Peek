f95c8da6b1ad2374f6c5b150eba892f95de14694
R3tr0LastKnight <shubh14shahu@gmail.com>
2024-07-09 16:39:51 +0530

    cart n wishlist

--- client/src/index.js
+++ client/src/index.js
@@ -6,12 +6,15 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <AuthProvider>
       <GoogleOAuthProvider clientId="11348802405-f8mp3d9tbf5f839mh7qf1k6oo9csec36.apps.googleusercontent.com">
+        <ToastContainer theme="dark" autoClose={900} />
         <App />
       </GoogleOAuthProvider>
     </AuthProvider>
